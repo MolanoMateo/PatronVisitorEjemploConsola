f468c72b156d5ff7c169204dba329443a4a8e362
RR <mateomolanov@outlook.com>
2023-01-27 08:15:33 -0500

    Add project files.

---
+++ VisitorVideo1/Componente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronVisitor
+{
+    public abstract class Componente
+    {
+        string _serial;
+
+        public Componente(string serial)
+        {
+            _serial = serial;
+        }
+        public string Serial { 
+            get {
+                return _serial;
+            }
+        }
+        public  void Aceptar(IVisitor visitor)
+        {
+            visitor.Visitar(_serial);
+        }
+    }
+}
---
+++ VisitorVideo1/IVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronVisitor
+{
+    public interface IVisitor
+    {
+        void Visitar(string serie);
+        
+
+    }
+}
